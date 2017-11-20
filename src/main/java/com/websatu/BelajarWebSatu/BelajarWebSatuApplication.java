65f235174d33797ce30dbe0f8ddb261f2a1b2d9d
FreezArt <snurisman@gmail.com>
2017-11-20 23:38:46 +0700

    Commit pertama spring boot

---
+++ src/main/java/com/websatu/BelajarWebSatu/BelajarWebSatuApplication.java
@@ -0,0 +1,12 @@
+package com.websatu.BelajarWebSatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarWebSatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarWebSatuApplication.class, args);
+	}
+}
